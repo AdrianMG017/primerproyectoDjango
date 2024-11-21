5f61200b816fb3787752b75aced578118da82ab9
AdrianMG017 <xamongon474@ieshnosmachado.org>
2024-11-21 12:39:48 +0100

    primer commit

--- primerproyecto/settings.py
+++ primerproyecto/settings.py
@@ -112,12 +112,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cualquier dominio o ip desde la que se quiera acceder']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
