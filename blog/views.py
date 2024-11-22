22d506c0a4ae56a7e48f87b317ced5bf35284442
AdrianMG017 <xamongon474@ieshnosmachado.org>
2024-11-22 12:05:21 +0100

    ultimo commit

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def principal(request):
     return render(request, 'principal.html')
+
+def segunda(request):
+    return render(request, 'segunda.html')
