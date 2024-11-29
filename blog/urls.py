69c5ad8f907b5ebe23ec5edac126bf0ac49ce7e5
AdrianMG017 <xamongon474@ieshnosmachado.org>
2024-11-29 12:53:37 +0100

    mas avance

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.principal, name='principal' ),
     path('<int:pk>', views.detalle_post, name='detalle_post'),
+    path('autores', views.autores, name='autores'),
+    path('autor/<int:pk>', views.detalle_autor, name='detalle_autor'),
     path('segunda', views.segunda, name='segunda')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post,Autor
 # Create your views here.
 def principal(request):
     context = Post.objects.all()
-    autores = Post.objects.values_list('autor',flat=True).distinct()
+    autores = Autor.objects.all()
     return render(request, 'blog/principal.html', {'posts':context,'autores':autores})
 
 def detalle_post(request, pk):
@@ -12,3 +12,12 @@ def detalle_post(request, pk):
 
 def segunda(request):
     return render(request, 'blog/segunda.html')
+
+def autores(request):
+    autores = Autor.objects.all()
+    return render(request, 'blog/autores.html', {'autores':autores})
+
+def detalle_autor(request,pk):
+    autor= get_object_or_404(Autor, pk=pk)
+    postDeAutores = Post.objects.filter(autor=pk)
+    return render(request, 'blog/detalle_autor.html', {'autor':autor,'posts':postDeAutores})
